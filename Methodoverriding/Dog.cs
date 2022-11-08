36628a117fec504d47f7f1bb859787025cc9c30b
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-09 01:38:26 +0530

    Methodoverriding

---
+++ Methodoverriding/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodoverriding
+{
+    class Dog : Animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("The dog says : bow bow");
+        }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Animal myAnimal = new Animal();
+                Pig myPig = new Pig();
+                Dog myDog = new Dog();
+                myAnimal.animalsound();
+                myPig.animalsound();
+                myDog.animalsound();
+            }
+        }
+    }
+}
---
+++ Methodoverriding/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Methodoverriding
+{
+    class Animal
+    {
+        public virtual void animalsound()
+        {
+            Console.WriteLine("The animal make a sound");
+        }
+    }
+}
